61534a8a8069b0386818576457ed5143a69dc209
MadinventorZero <dmafduke@gmail.com>
2021-04-08 12:35:57 -0700

    Progress on making a unit test for Nissan

--- __tests__/unitTest.js
+++ __tests__/unitTest.js
@@ -1,7 +1,20 @@
 const { nissan } = require('../client/containers/Nissan');
 
-xdescribe('Nissan Unit Tests', () => {
+describe('Nissan Unit Tests', () => {
+  let state;
+  
+  beforeEach(() => {
+    state = {
+      board: {},
+      headPosition: '0,0',
+      targetPosition: '9,9',
+      path: [],
+      onFire: [],
+    },
+    setState = (...state) => {
 
+    };
+  });
   describe('Put tests here', () => {});
   xdescribe('Put tests here', () => {});
   xdescribe('Put tests here', () => {});
