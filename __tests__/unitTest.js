4273429ced3be3a9eb42a078db65646b520d6d25
MadinventorZero <dmafduke@gmail.com>
2021-04-08 15:48:13 -0700

    Progress on testing the Algorithm, made a testData file and using John's setState remake

--- __tests__/unitTest.js
+++ __tests__/unitTest.js
@@ -1,21 +1,28 @@
 const { nissan } = require('../client/containers/Nissan');
+const testData = require('../__tests__/testData');
 
 describe('Nissan Unit Tests', () => {
   let state;
   
+  function setState(updateObj) {
+    for (const key in updateObj) {
+      state.key = updateObj[key];
+    };
+  };
+
   beforeEach(() => {
     state = {
-      board: {},
+      board: JSON.parse(JSON.stringify(testData.initialBoardState)),
       headPosition: '0,0',
       targetPosition: '9,9',
       path: [],
       onFire: [],
     },
-    setState = (...state) => {
-
-    };
   });
-  describe('Put tests here', () => {});
+
+  describe('Put tests here', () => {
+
+  });
   xdescribe('Put tests here', () => {});
   xdescribe('Put tests here', () => {});
   xdescribe('Put tests here', () => {});
