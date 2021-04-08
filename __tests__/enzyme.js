ac6bbc9e908dd3d2bad1972b47a3f9bd2b556885
Danny <codesmith@jdanielmartinez.com>
2021-04-08 12:05:53 -0700

    Add skeleton for Nav button tests

--- __tests__/enzyme.js
+++ __tests__/enzyme.js
@@ -3,12 +3,23 @@ import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import toJson from 'enzyme-to-json';
 
+import Navbar from '../client/Navbar';
+
 configure({adapter: new Adapter()});
 
-describe('All tests', () => {
+describe('All enzyme tests', () => {
   describe('Put tests here', () => {});
   describe('Put tests here', () => {});
   describe('Put tests here', () => {});
   describe('Put tests here', () => {});
-  describe('Put tests here', () => {});
+  describe('Nav Button Tests', () => {
+    let wrapper;
+    const props = {
+      
+    };
+    beforeAll(() => {
+      wrapper = shallow(<Navbar {...props} />);
+    });
+
+  });
 });
