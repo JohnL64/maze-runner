aa310921013255add66028d91c2717f168fe466e
Danny <codesmith@jdanielmartinez.com>
2021-04-08 11:23:19 -0700

    Add boilerplate for jest testing

--- __tests__/enzyme.js
+++ __tests__/enzyme.js
@@ -1,1 +1,14 @@
-// React Tests File
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import toJson from 'enzyme-to-json';
+
+configure({adapter: new Adapter()});
+
+describe('All tests', () => {
+  describe('Put tests here', () => {});
+  describe('Put tests here', () => {});
+  describe('Put tests here', () => {});
+  describe('Put tests here', () => {});
+  describe('Put tests here', () => {});
+});
--- __tests__/supertest.js
+++ __tests__/supertest.js
@@ -1,1 +1,14 @@
 // SERVER TESTS FILE
+const {response} = require('express');
+const request = require('supertest');
+const db = require('../server/models/postgresModel');
+const server = 'http://localhost:3010';
+
+
+describe('All tests', () => {
+  describe('Put tests here', () => {});
+  describe('Put tests here', () => {});
+  describe('Put tests here', () => {});
+  describe('Put tests here', () => {});
+  describe('Put tests here', () => {});
+});
