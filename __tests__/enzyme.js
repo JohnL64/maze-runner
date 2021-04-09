c28e6aef6171c7fca42e68e3e6fea9ae6c5831af
Tobey Forsman <tobeyforsman@gmail.com>
2021-04-08 17:50:53 -0700

    Added tests for Button component

--- __tests__/enzyme.js
+++ __tests__/enzyme.js
@@ -3,12 +3,35 @@ import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import toJson from 'enzyme-to-json';
 
+import Board from '../client/containers/board';
+import Button from '../client/containers/button';
+
 configure({adapter: new Adapter()});
 
-xdescribe('All tests', () => {
-  xdescribe('Put tests here', () => {});
-  xdescribe('Put tests here', () => {});
-  xdescribe('Put tests here', () => {});
-  xdescribe('Put tests here', () => {});
-  xdescribe('Put tests here', () => {});
+describe('All enzyme Board/Button tests', () => {
+  describe('Board/Button tests', () => {
+    let wrapper;
+
+    const props = {
+      className: '',
+      id: '',
+      onMouseDown: jest.fn(),
+      onMouseOver: jest.fn(),
+      onClick: jest.fn()
+    };
+
+    beforeAll(() => {
+      wrapper = shallow(<Button {...props} />);
+    });
+
+    it('Should generate am HTML button', () => {
+      expect(wrapper.find('button')).toHaveLength(1);
+    });
+
+    it('Should execute passed in function when clicked', () => {
+      wrapper.find('button').simulate('click');
+      expect(props.onClick).toHaveBeenCalled();
+    });
+
+  });
 });
